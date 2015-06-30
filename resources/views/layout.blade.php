5f2dcf1ddb438f2b34d6de6e5952048377d8fd70
Goose <goosepostbox@gmail.com>
2015-06-29 20:40:48 -0400

    Favicon and title
    Loose ends while I wrap up this iteration.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Steve Thinker | Chat Bot</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/global_style.css') }}" rel="stylesheet">
@@ -19,6 +19,8 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<!-- Favicon Icon -->
+    <link rel="shortcut icon" href="favicon.ico">
 </head>
 <body>
 
