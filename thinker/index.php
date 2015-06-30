8c28d6601ffc5e8ad80a125581769773f1560cf7
Goose <goosepostbox@gmail.com>
2015-06-29 20:14:24 -0400

    Project renamed to steve and public is now thinker
    Root folder is called steve, public redirected and renamed thinker,
    which is where the public files are moved to.

--- thinker/index.php
+++ thinker/index.php
@@ -34,10 +34,6 @@ require __DIR__.'/../bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// set the public path to this directory
-$app->bind('path.public', function() {
-    return '../thinker';
-});
 /*
 |--------------------------------------------------------------------------
 | Run The Application
